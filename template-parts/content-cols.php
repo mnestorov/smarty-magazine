2617d535502ad81e5da296a60d81e2e6e83251db
Martin Nestorov <mbnestorov@gmail.com>
2025-03-04 01:29:40 +0200

    Add tabbed navigation for content display and adjust column layout for improved responsiveness

--- template-parts/content-cols.php
+++ template-parts/content-cols.php
@@ -13,16 +13,39 @@
         <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
     </header>
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-between">
             <!-- Left Column (Main Content) -->
-            <div class="col-lg-6 col-md-6 px-0">
+            <div class="col-lg-5 col-md-6 px-0">
                 <div class="entry-content">
                     <?php wpautop(the_content()); ?>
                 </div>
             </div>
             <!-- Right Column (Sidebar / Additional Content) -->
-            <div class="col-lg-6 col-md-6 px-0">
-                
+            <div class="col-lg-6 col-md-6 my-3 px-0">
+                <div class="nav-pills-container mx-auto">
+                    <ul class="nav nav-underline mb-4" id="pills2-tab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link text-uppercase active" id="pills2-tab1-tab" data-bs-toggle="pill" href="#pills2-tab1" role="tab" aria-controls="pills2-tab1" aria-selected="true">
+                                <?php _e('Chain Dominance', 'smarty-magazine'); ?>
+                            </a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link text-uppercase" id="pills2-tab2-tab" data-bs-toggle="pill" href="#pills2-tab2" role="tab" aria-controls="pills2-tab2" aria-selected="false">
+                                <?php _e('Currencies Dominance', 'smarty-magazine'); ?>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab Content -->
+                    <div class="tab-content sm-tab-content" id="pills2-tabContent">
+                        <div class="tab-pane fade show active" id="pills2-tab1" role="tabpanel" aria-labelledby="pills2-tab1-tab">
+                            <?php echo do_shortcode('[smarty_cdg_tvl_chain_dominance]'); ?>
+                        </div>
+                        <div class="tab-pane fade" id="pills2-tab2" role="tabpanel" aria-labelledby="pills2-tab2-tab">
+                            <?php echo do_shortcode('[smarty_cdg_currencies_dominance]'); ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
